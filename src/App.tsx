d2f02cf84224073f1f43f195a2f9c1c40c406113
Gabriel Moreira <gabrielms.cec@gmail.com>
2023-08-24 22:47:47 -0300

    adiciona emoji

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Render from './components/Render';
 function App() {
   return (
     <div>
-      <h1>Gerenciador de senhas</h1>
+      <h1>Gerenciador de senhas 🔒 </h1>
       <Render />
     </div>
   );
